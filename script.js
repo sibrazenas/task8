22a51e4c871627eac0d0ce6c35d2c879a5054db2
Sigitas Brazenas <sigitasbrazenas@Sigitass-Air.lan>
2022-08-21 18:31:37 +0300

    added constructor functions

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+function Calculator(a,b) {
+    this.a = a
+    this.b = b
+
+
+    this.sum = function (a,b) {
+       console.log(parseInt(a) + parseInt(b))
+    }
+    this.substraction= function (a,b) {
+        console.log(parseInt(a) - parseInt(b))
+     }
+     this.multiplication = function (a,b) {
+        console.log(parseInt(a) * parseInt(b))
+     }
+     this.division = function (a,b) {
+        console.log(parseInt(a) / parseInt(b))
+     }
+}
+
+const calculator1 = new Calculator()
+
+calculator1.sum(20,20) // 40
+calculator1.division(10,5) // 2
+calculator1.multiplication(2,2) // 4
+calculator1.substraction(9,3) // 6
+
